c0017d6c334952ab7c1bee7b67f177060877a44e
damsarar <damsarar@gmail.com>
2020-03-25 15:58:44 +0530

    Updates

---
+++ precache-manifest.b40e414d73feaedc4c5c77f3c5ca8b8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8759d1ef1abc8f37617d56b2eab8333",
+    "url": "/damsarar/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "8ae1416ddbaeaa08b962",
+    "url": "/damsarar/Covid19-Tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8bc12971d71e68120694",
+    "url": "/damsarar/Covid19-Tracker/static/css/main.b5e9d419.chunk.css"
+  },
+  {
+    "revision": "8ae1416ddbaeaa08b962",
+    "url": "/damsarar/Covid19-Tracker/static/js/2.148794ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/damsarar/Covid19-Tracker/static/js/2.148794ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc12971d71e68120694",
+    "url": "/damsarar/Covid19-Tracker/static/js/main.dc74c11b.chunk.js"
+  },
+  {
+    "revision": "8f1308b7f9d3d5518091",
+    "url": "/damsarar/Covid19-Tracker/static/js/runtime-main.9757274b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/damsarar/Covid19-Tracker/precache-manifest.b40e414d73feaedc4c5c77f3c5ca8b8d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/damsarar/Covid19-Tracker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
